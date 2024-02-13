e0607e6f069c839fb4d43afd4bb09f3a95253472
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-14 00:17:34 +0530

    react-youtube-app_1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Navbar from "./components/navbar/Navbar"
-
+import { Routes, Route } from 'react-router-dom'
+import Home from "./pages/home/Home"
+import Video from "./pages/video/Video"
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
+import Sidebar from '../../components/sidebar/Sidebar'
 import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Sidebar />
+    </>
   )
 }
 
