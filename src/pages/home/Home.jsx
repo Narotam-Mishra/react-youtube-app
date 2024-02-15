55ff9d380af44e6927b0cf4360f0cdf88df00d39
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-15 23:36:41 +0530

    react-youtube-app_1.8

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
+import Feed from '../../components/Feed/Feed'
 import Sidebar from '../../components/sidebar/Sidebar'
 import './Home.css'
 
-const Home = ( { sidebar }) => {
+const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar}/>
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   )
 }
