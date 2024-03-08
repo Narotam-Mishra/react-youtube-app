e69b0dc1283006bc0be1c824ba0e7b4f69914834
Narotam-Mishra <narotam21jan@gmail.com>
2024-03-08 23:17:29 +0530

    react-youtube-app_1.17

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,42 +8,9 @@ import more_icon from '../../assets/more.png'
 import notification_icon from '../../assets/notification.png'
 import profile_icon from '../../assets/jack.png'
 import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-
-const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
 
 const Navbar = ({ setSidebar }) => {
-  const [data, setData] = useState([]);
-  // State to hold the search query
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
-
-  const fetchData = async () => {
-    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=0&key=${apiKey}`;
-    await fetch(videoList_url)
-      .then((response) => response.json())
-      .then((data) => setData(data.items));
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    // Filter the data based on the search query
-    const filtered = data.filter(item =>
-      // Check if item.title is defined before calling toLowerCase()
-      item.title && item.title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setFilteredData(filtered);
-  }, [searchQuery, data]);
-
-
-  // Event handler to update the search query state when input changes
-  const handleSearchInputChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
+  
   return (
     <nav className="flex-div">
       <div className="nav-left flex-div">
@@ -63,8 +30,6 @@ const Navbar = ({ setSidebar }) => {
           <input
             type="text"
             placeholder="Search"
-            value={searchQuery}
-            onChange={handleSearchInputChange}
           />
           <img src={search_icon} alt="Search icon" />
         </div>
@@ -75,16 +40,6 @@ const Navbar = ({ setSidebar }) => {
         <img src={more_icon} alt="more icon" />
         <img src={notification_icon} alt="notification icon" />
         <img src={profile_icon} className="user-icon" alt="profile icon" />
-      </div>
-
-      <div>
-        {/* Render filtered data */}
-        {filteredData.map(item => (
-          <div key={item.id}>
-            {/* Render item details here */}
-            <p>{item.title}</p>
-          </div>
-        ))}
       </div>
     </nav>
   );
